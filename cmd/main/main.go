062ff93c98b7fc7be5c8861c2b81b8361971d519
Roland <roland.burke@gmx.de>
2023-06-03 13:32:09 +0200

    added another module

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,155 +1,24 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
 	"time"
 
 	"github.com/roland-burke/local-network-overview/internal/model"
+	"github.com/roland-burke/local-network-overview/internal/server"
 	"github.com/roland-burke/rollogger"
 )
 
-var currentState model.AllHostsResponse
-var logger *rollogger.Log
-
-const port = "8080"
-
-func checkSingleAvailability(host model.NetClient) int {
-	client := http.Client{
-		Timeout: 4 * time.Second,
-	}
-
-	res, err := client.Get("http://" + host.AlternativeHost)
-	if err != nil {
-		logger.Info("Failed: %s", err.Error())
-		// Connection not established
-		return 1
-	}
-
-	if res.StatusCode >= 200 && res.StatusCode < 300 {
-		// All fine
-		return 0
-	}
-
-	// Service reachabled, but no 2xx response
-	return 2
-}
-
-func checkAvailability() model.AllHostsResponse {
-	loadedConfig, err := loadConfig()
-
-	if err != nil {
-		logger.Warn("Error during config load: " + err.Error())
-		return model.AllHostsResponse{
-			Status:    1,
-			StatusMsg: err.Error(),
-		}
-	}
-
-	var hosts = loadedConfig.Clients
-
-	var hostStatusList []model.SingleHostStatus
-
-	logger.Info("Start availability check...")
-	for i := 0; i < len(hosts); i++ {
-		logger.Info("Check %s (%s)", hosts[i].HostIp, hosts[i].AlternativeHost)
-		available := checkSingleAvailability(hosts[i])
-
-		var availValue = "UNKNOWN"
-
-		if available == 0 {
-			availValue = "UP"
-		} else if available == 1 {
-			availValue = "DOWN"
-		} else if available == 2 {
-			availValue = "PROBLEM"
-		}
-
-		var status = model.SingleHostStatus{
-			Client: hosts[i],
-			Status: availValue,
-		}
-		hostStatusList = append(hostStatusList, status)
-	}
-
-	return model.AllHostsResponse{
-		Status:    0,
-		StatusMsg: "okay",
-		Data:      hostStatusList[:],
-		TimeStamp: time.Now(),
-	}
-}
-
-func returnCurrentState(w http.ResponseWriter, r *http.Request) {
-	json, err := json.Marshal(currentState)
-	if err != nil {
-		fmt.Printf(err.Error())
-		w.Write([]byte("Failed to marshal result: " + err.Error()))
-	} else {
-		w.Write(json)
-	}
-}
-
-func startServer() {
-	http.Handle("/", http.FileServer(http.Dir("./static")))
-
-	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
-		returnCurrentState(w, r)
-	})
-
-	http.HandleFunc("/status/now", func(w http.ResponseWriter, r *http.Request) {
-		currentState = checkAvailability()
-		returnCurrentState(w, r)
-	})
-
-	var err = http.ListenAndServe(":"+port, nil)
-	logger.Error(err.Error())
-}
-
-func loadConfig() (model.ConfFile, error) {
-	// Open our jsonFile
-	jsonFile, err := os.Open("conf/config.json")
-	// if we os.Open returns an error then handle it
-	if err != nil {
-		return model.ConfFile{}, errors.New("Cannot open file: " + err.Error())
-	}
-
-	// defer the closing of our jsonFile so that we can parse it later on
-	defer jsonFile.Close()
-
-	byteValue, err := ioutil.ReadAll(jsonFile)
-	if err != nil {
-		return model.ConfFile{}, errors.New("Cannot read file: " + err.Error())
-	}
-	var configFile model.ConfFile
-
-	err = json.Unmarshal(byteValue, &configFile)
-
-	if err != nil {
-		return model.ConfFile{}, errors.New("Cannot unmarshal json: " + err.Error())
-	}
-	logger.Info("Configured with %d targets and a retry intervall of %ds.", len(configFile.Clients), configFile.RetryIntervall)
-	return configFile, nil
-}
-
-func executeTimedRequest() {
-	currentState = checkAvailability()
-}
-
 func main() {
-	logger = rollogger.Init(rollogger.INFO_LEVEL, true, true)
-	currentState = model.AllHostsResponse{
+	server.Logger = rollogger.Init(rollogger.INFO_LEVEL, true, true)
+	server.CurrentState = model.AllHostsResponse{
 		Status:    2,
 		StatusMsg: "Check was not performed yet.",
 	}
 
-	var loadedConfig, err = loadConfig()
+	var loadedConfig, err = server.LoadConfig()
 	if err != nil {
-		logger.Error(err.Error())
+		server.Logger.Error(err.Error())
 		os.Exit(1)
 	}
 	uptimeTicker := time.NewTicker(time.Duration(loadedConfig.RetryIntervall) * time.Second)
@@ -158,10 +27,10 @@ func main() {
 		for {
 			select {
 			case <-uptimeTicker.C:
-				go executeTimedRequest()
+				go server.ExecuteTimedRequest()
 			}
 		}
 	}()
 
-	startServer()
+	server.StartServer()
 }
---
+++ internal/server/server.go
@@ -0,0 +1,141 @@
+package server
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/roland-burke/local-network-overview/internal/model"
+	"github.com/roland-burke/rollogger"
+)
+
+var CurrentState model.AllHostsResponse
+var Logger *rollogger.Log
+
+const port = "8080"
+
+func checkSingleAvailability(host model.NetClient) int {
+	client := http.Client{
+		Timeout: 4 * time.Second,
+	}
+
+	res, err := client.Get("http://" + host.AlternativeHost)
+	if err != nil {
+		Logger.Info("Failed: %s", err.Error())
+		// Connection not established
+		return 1
+	}
+
+	if res.StatusCode >= 200 && res.StatusCode < 300 {
+		// All fine
+		return 0
+	}
+
+	// Service reachabled, but no 2xx response
+	return 2
+}
+
+func checkAvailability() model.AllHostsResponse {
+	loadedConfig, err := LoadConfig()
+
+	if err != nil {
+		Logger.Warn("Error during config load: " + err.Error())
+		return model.AllHostsResponse{
+			Status:    1,
+			StatusMsg: err.Error(),
+		}
+	}
+
+	var hosts = loadedConfig.Clients
+
+	var hostStatusList []model.SingleHostStatus
+
+	Logger.Info("Start availability check...")
+	for i := 0; i < len(hosts); i++ {
+		Logger.Info("Check %s (%s)", hosts[i].HostIp, hosts[i].AlternativeHost)
+		available := checkSingleAvailability(hosts[i])
+
+		var availValue = "UNKNOWN"
+
+		if available == 0 {
+			availValue = "UP"
+		} else if available == 1 {
+			availValue = "DOWN"
+		} else if available == 2 {
+			availValue = "PROBLEM"
+		}
+
+		var status = model.SingleHostStatus{
+			Client: hosts[i],
+			Status: availValue,
+		}
+		hostStatusList = append(hostStatusList, status)
+	}
+
+	return model.AllHostsResponse{
+		Status:    0,
+		StatusMsg: "okay",
+		Data:      hostStatusList[:],
+		TimeStamp: time.Now(),
+	}
+}
+
+func returnCurrentState(w http.ResponseWriter, r *http.Request) {
+	json, err := json.Marshal(CurrentState)
+	if err != nil {
+		fmt.Printf(err.Error())
+		w.Write([]byte("Failed to marshal result: " + err.Error()))
+	} else {
+		w.Write(json)
+	}
+}
+
+func StartServer() {
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+
+	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		returnCurrentState(w, r)
+	})
+
+	http.HandleFunc("/status/now", func(w http.ResponseWriter, r *http.Request) {
+		CurrentState = checkAvailability()
+		returnCurrentState(w, r)
+	})
+
+	var err = http.ListenAndServe(":"+port, nil)
+	Logger.Error(err.Error())
+}
+
+func LoadConfig() (model.ConfFile, error) {
+	// Open our jsonFile
+	jsonFile, err := os.Open("conf/config.json")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		return model.ConfFile{}, errors.New("Cannot open file: " + err.Error())
+	}
+
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return model.ConfFile{}, errors.New("Cannot read file: " + err.Error())
+	}
+	var configFile model.ConfFile
+
+	err = json.Unmarshal(byteValue, &configFile)
+
+	if err != nil {
+		return model.ConfFile{}, errors.New("Cannot unmarshal json: " + err.Error())
+	}
+	Logger.Info("Configured with %d targets and a retry intervall of %ds.", len(configFile.Clients), configFile.RetryIntervall)
+	return configFile, nil
+}
+
+func ExecuteTimedRequest() {
+	CurrentState = checkAvailability()
+}
